506b90e942b554a67d3ab45a4ef1840ea0ef03eb
helloworld <18989893637@189.cn>
2017-04-29 23:26:53 +0800

    对单实例这块加了javadock注释.增加了使用例子。

--- TUtil/CSingleton.h
+++ TUtil/CSingleton.h
@@ -1,14 +1,37 @@
 #ifndef CSINGLETON
 #define CSINGLETON
+
+
 namespace Taiji
 {
 
 namespace TUtil {
 
-/*
- * @Param: index  使用 index 可以对同一个类生成不同的单实例
+/**
+ * @brief 把某个类变为单实例的模板类
+ * @tparam T 要做为单实例的类
+ * @tparam index 使用 index 可以对同一个类生成不同的单实例
+ * @example
  *
- * @eg: CSingleton<MyClass,0>  CSingleton<MyClass, 1>
+ * 下面对 CLog 给每个模块做一个单实例
+ *
+ * cpp 文件里显示实例化:
+ *  template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 0>;
+ *  template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 1>;
+ *  template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 2>;
+ *  template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 3>;
+ *
+ * h 文件模板声明:
+ *  extern template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 0>;
+ *  extern template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 1>;
+ *  extern template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 2>;
+ *  extern template class Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 3>;
+ *
+ * 重命名模板类型，方便使用
+ *  using LogApplicaton      = Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 0>;
+ *  using LogRedis           = Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 1>;
+ *  using LogKafka           = Taiji::TUtil::CSingleton<Taiji::TUtil::CLog, 2>;
+ *
  */
 template<typename T, int index = 0>
 class CSingleton
